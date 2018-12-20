6b41dec539163479d5c64122612bf315933512e5
bastaiguer <velkozismygod@gmail.com>
2018-12-20 14:38:48 +0100

    Modelo iniciado

--- app/src/main/java/com/simarro/joshu/clientsqr/Activities/MainActivity.java
+++ app/src/main/java/com/simarro/joshu/clientsqr/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.simarro.joshu.clientsqr;
+package com.simarro.joshu.clientsqr.Activities;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +7,8 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.simarro.joshu.clientsqr.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
