49a6b5369ba8aae96db5cc40331e3e367caac95d
bastaiguer <velkozismygod@gmail.com>
2018-12-20 14:25:46 +0100

    Login hecho

--- app/src/main/java/com/simarro/joshu/clientsqr/MainActivity.java
+++ app/src/main/java/com/simarro/joshu/clientsqr/MainActivity.java
@@ -1,13 +1,24 @@
 package com.simarro.joshu.clientsqr;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
